bd26d8b59e35ef03f063a947fdd362568b520abe
Vinicius Tabille Manjabosco <978715+thevtm@users.noreply.github.com>
2026-03-03 12:08:54 +0100

    2025: Day 01 Part 2

--- advent-of-code-2025/day-01/solution.rb
+++ advent-of-code-2025/day-01/solution.rb
@@ -15,14 +15,24 @@ $rotations = $file_lines.map do |n|
 end
 
 $dial_positions = $rotations.map do |r|
-  $dial = (100 + $dial + r) % 100
+  $dial = ($dial + r) % 100
   $dial
 end
 
 $dial_zero_count = $dial_positions.count(0)
 
-puts "Problem 1 Result: #{$dial_zero_count}"
+puts "Problem 1 Result: #{$dial_zero_count}" # 1132
 
 # PROBLEM 2
 
-# puts "Problem 2 Result:"
+$dial = 50
+$dial_pass_by_zero_count = 0
+
+$rotations.each do |r|
+  (1..r.abs).each do
+    $dial = ($dial + (r > 0 ? 1 : -1)) % 100
+    $dial_pass_by_zero_count += 1 if $dial == 0
+  end
+end
+
+puts "Problem 2 Result: #{$dial_pass_by_zero_count}" # 6623
