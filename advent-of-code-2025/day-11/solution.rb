f82022333618e50c0554e441080b8658e0b91759
Vinicius Tabille Manjabosco <978715+thevtm@users.noreply.github.com>
2026-03-14 19:46:00 +0100

    2025: Day 11 - Part 2

--- advent-of-code-2025/day-11/solution.rb
+++ advent-of-code-2025/day-11/solution.rb
@@ -1,7 +1,7 @@
 # INPUTS
 
 $input_path = './input.txt'
-# $input_path = "./input-sample.txt"
+# $input_path = "./input-sample-part-1.txt"
 
 $file_content = File.read(File.join(__dir__, $input_path))
 $file_lines = $file_content.lines(chomp: true)
@@ -20,16 +20,43 @@ timer_start = Time.now
 $cache = {}
 $cache['out'] = 1
 
-def path_count_from(server_rack)
-  $cache[server_rack] ||= $graph[server_rack].map { |x| path_count_from(x) }.sum
+def path_count(server_rack)
+  $cache[server_rack] ||= $graph[server_rack].map { |x| path_count(x) }.sum
 end
 
-$paths_count = path_count_from('you')
+$paths_count = path_count('you')
 
 puts "Problem 1 Result: #{$paths_count} ● #{format('%.1f', Time.now - timer_start)}s" # 688 ● 0.0s
 
 # PROBLEM 2
 
-# timer_start = Time.now
+# $input_path = "./input-sample-part-2.txt"
 
-# puts "Problem 2 Result: #{0} ● #{format('%.1f', Time.now - timer_start)}s" # 19210 ● 0.3s
+$file_content = File.read(File.join(__dir__, $input_path))
+$file_lines = $file_content.lines(chomp: true)
+
+$graph = $file_lines.each_with_object({}) do |line, acc|
+  source = line[...3]
+  targets = line[4..].split
+
+  acc[source] = targets
+end
+
+timer_start = Time.now
+
+$cache = {}
+
+def path_count(from, to, seen = Set.new)
+  return 1 if from == to
+  return 0 if from == 'out'
+
+  $cache["#{from}-#{to}"] ||= $graph[from].reject { |x| seen.include?(x) }.map { |x| path_count(x, to, seen) }.sum
+end
+
+$svr_to_fft = path_count('svr', 'fft')
+$fft_to_dac = path_count('fft', 'dac')
+$dac_to_out = path_count('dac', 'out')
+
+$paths_count = $svr_to_fft * $fft_to_dac * $dac_to_out
+
+puts "Problem 2 Result: #{$paths_count} ● #{format('%.1f', Time.now - timer_start)}s" # 293263494406608 ● 0.0s
