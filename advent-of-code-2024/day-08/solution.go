22c35c7724f3428bfdd95f4c61f70d46f2b92dcc
Vinicius Tabille Manjabosco <978715+thevtm@users.noreply.github.com>
2026-03-24 18:56:37 +0100

    2024: Day 8 - Part 2

--- advent-of-code-2024/day-08/solution.go
+++ advent-of-code-2024/day-08/solution.go
@@ -6,31 +6,35 @@ import (
 	"path/filepath"
 	"runtime"
 	"strings"
+	"time"
 
 	"github.com/samber/lo"
 )
 
-type Position struct {
+type Point struct {
 	x, y int
 }
 
+type Line struct {
+	a, b Point
+}
+
 type Antenna struct {
-	position  Position
+	position  Point
 	frequency rune
 }
 
-func absInt(x int) int {
-	if x < 0 {
-		return -x
-	}
-	return x
+func pointInLine(point Point, line Line) bool {
+	// cross product of (B-A) x (P-A) == 0 means collinear
+	cross := (line.b.x-line.a.x)*(point.y-line.a.y) - (line.b.y-line.a.y)*(point.x-line.a.x)
+	return cross == 0
 }
 
 func main() {
 	// INPUTS
 
 	input_path := "./input.txt"
-	// input_path := "./input-sample.txt"
+	// input_path = "./input-sample.txt"
 
 	_, source_file_path, _, _ := runtime.Caller(0)
 	source_dir := filepath.Dir(source_file_path)
@@ -44,13 +48,13 @@ func main() {
 	file_lines = file_lines[:len(file_lines)-1] // Last line is blank
 
 	antennas := make([]Antenna, 0)
-	taken_positions := make(map[Position]bool)
+	taken_positions := make(map[Point]bool)
 
 	for y, line := range file_lines {
 		for x, cell := range line {
 			if cell != '.' {
-				taken_positions[Position{x, y}] = true
-				antennas = append(antennas, Antenna{Position{x, y}, cell})
+				taken_positions[Point{x, y}] = true
+				antennas = append(antennas, Antenna{Point{x, y}, cell})
 			}
 		}
 	}
@@ -70,7 +74,9 @@ func main() {
 
 	// PROBLEM 1
 
-	is_in_bounds := func(pos Position) bool {
+	start := time.Now()
+
+	is_in_bounds := func(pos Point) bool {
 		if pos.x < 0 || pos.x >= map_width || pos.y < 0 || pos.y >= map_height {
 			return false
 		}
@@ -78,7 +84,7 @@ func main() {
 		return true
 	}
 
-	anti_node_unique_positions := make(map[Position]bool)
+	anti_node_unique_positions := make(map[Point]bool)
 
 	antennas_by_frequency := lo.GroupBy(antennas, func(antenna Antenna) rune {
 		return antenna.frequency
@@ -96,8 +102,8 @@ func main() {
 				x := a.position.x - b.position.x
 				y := a.position.y - b.position.y
 
-				aa := Position{a.position.x + x, a.position.y + y}
-				ab := Position{b.position.x - x, b.position.y - y}
+				aa := Point{a.position.x + x, a.position.y + y}
+				ab := Point{b.position.x - x, b.position.y - y}
 
 				if is_in_bounds(aa) {
 					anti_node_unique_positions[aa] = true
@@ -112,5 +118,40 @@ func main() {
 
 	fmt.Println()
 
-	fmt.Println("Problem 1 Result:", len(anti_node_unique_positions)) // 256
+	fmt.Println("Problem 1 Result:", len(anti_node_unique_positions), "●", time.Since(start)) // 256 ● 117.573µs
+
+	fmt.Println()
+	fmt.Println("--------------------------------------------------------------------------------")
+	fmt.Println()
+
+	// Problem 2
+
+	start = time.Now()
+
+	lines := make([]Line, 0)
+
+	for _, antennas := range antennas_by_frequency {
+		for i := 0; i < len(antennas)-1; i++ {
+			for j := i + 1; j < len(antennas); j++ {
+				lines = append(lines, Line{antennas[i].position, antennas[j].position})
+			}
+		}
+	}
+
+	anti_node_unique_positions = make(map[Point]bool)
+
+	for i := 0; i < map_width; i++ {
+		for j := 0; j < map_height; j++ {
+			point := Point{i, j}
+
+			for _, line := range lines {
+				if pointInLine(point, line) {
+					anti_node_unique_positions[point] = true
+					break
+				}
+			}
+		}
+	}
+
+	fmt.Println("Problem 2 Result:", len(anti_node_unique_positions), "●", time.Since(start)) // 1005 ● 832.328µs
 }
