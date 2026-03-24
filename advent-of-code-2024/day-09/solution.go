ddcb271d5a331146f7f793d6a000db56e6f13f26
Vinicius Tabille Manjabosco <978715+thevtm@users.noreply.github.com>
2026-03-24 21:08:38 +0100

    2024: Day 9 - Part 1 - Remake

--- advent-of-code-2024/day-09/solution.go
+++ advent-of-code-2024/day-09/solution.go
@@ -5,22 +5,16 @@ import (
 	"os"
 	"path/filepath"
 	"runtime"
+	"time"
 
 	"github.com/samber/lo"
 )
-
-func absInt(x int) int {
-	if x < 0 {
-		return -x
-	}
-	return x
-}
 
 func main() {
 	// INPUTS
 
 	input_path := "./input.txt"
-	// input_path := "./input-sample.txt"
+	// input_path = "./input-sample.txt"
 
 	_, source_file_path, _, _ := runtime.Caller(0)
 	source_dir := filepath.Dir(source_file_path)
@@ -34,30 +28,13 @@ func main() {
 
 	digit_map := map[byte]int{'0': 0, '1': 1, '2': 2, '3': 3, '4': 4, '5': 5, '6': 6, '7': 7, '8': 8, '9': 9}
 
-	var disk [100000]int
-	empty_sector_id := -1
-	index := 0
+	disk := make([]int, len(file_content))
 
-	for i := 0; i < len(file_content)/2; i++ {
-		file_size := digit_map[file_content[i*2]]
-		free_size := digit_map[file_content[(i*2)+1]]
-
-		for j := 0; j < file_size; j++ {
-			disk[index] = i
-			index++
-		}
-
-		for j := 0; j < free_size; j++ {
-			disk[index] = empty_sector_id
-			index++
-		}
+	for i := 0; i < len(file_content); i++ {
+		disk[i] = digit_map[file_content[i]]
 	}
 
-	fmt.Println(disk[:index])
-
-	fmt.Println()
-
-	fmt.Println("index", index)
+	fmt.Println("disk", disk)
 
 	fmt.Println()
 	fmt.Println("--------------------------------------------------------------------------------")
@@ -65,40 +42,57 @@ func main() {
 
 	// PROBLEM 1
 
-	left_index := 0
-	right_index := index - 1
+	start := time.Now()
 
-	for {
-		for ; disk[left_index] != empty_sector_id; left_index++ {
+	index := 0
+	left_block_index := 0
+	right_file_block_index := len(disk) - ((len(disk) - 1) % 2) - 1
+
+	disk_used := make([]int, len(disk))
+	copy(disk_used, disk)
+
+	checksum := int64(0)
+
+	for right_file_block_index >= left_block_index {
+		// fmt.Println("disk_used", disk_used)
+		// fmt.Println("\t", "index", index, "left_block_index", left_block_index, "right_file_block_index", right_file_block_index)
+
+		if disk_used[left_block_index] == 0 {
+			left_block_index++
+			continue
 		}
 
-		for ; disk[right_index] == empty_sector_id; right_index-- {
+		if disk_used[right_file_block_index] == 0 {
+			right_file_block_index -= 2
+			continue
 		}
 
-		if left_index < right_index {
-			break
+		is_empty_sector := (left_block_index % 2) == 1
+
+		if is_empty_sector {
+			disk_used[left_block_index]--
+			disk_used[right_file_block_index]--
+
+			file_id := right_file_block_index / 2
+
+			checksum += int64(index * file_id)
+		} else {
+			disk_used[left_block_index]--
+
+			file_id := left_block_index / 2
+
+			checksum += int64(index * file_id)
 		}
 
-		disk[left_index] = disk[right_index]
-		disk[right_index] = empty_sector_id
-
-		// fmt.Println(left_index, right_index, "\t", disk[:index])
-
-		left_index++
-		right_index--
+		index++
 	}
 
 	fmt.Println()
 
-	fmt.Println(disk[:index])
+	fmt.Println("Problem 1 Result:", checksum, "●", time.Since(start)) // 6399153661894 ● 209.369µs
 
 	fmt.Println()
+	fmt.Println("--------------------------------------------------------------------------------")
+	fmt.Println()
 
-	var checksum int64 = 0
-
-	for i := 0; disk[i] != empty_sector_id; i++ {
-		checksum += int64(i) * int64(disk[i])
-	}
-
-	fmt.Println("Problem 1 Result:", checksum) // 6399153661894
 }
