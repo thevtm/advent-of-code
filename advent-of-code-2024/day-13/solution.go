8bafc395098bd50e38136d06a66173cea5ab683b
Vinicius Tabille Manjabosco <978715+thevtm@users.noreply.github.com>
2026-04-03 19:58:46 +0200

    2024: Day 13 - Part 2

--- advent-of-code-2024/day-13/solution.go
+++ advent-of-code-2024/day-13/solution.go
@@ -7,6 +7,7 @@ import (
 	"regexp"
 	"runtime"
 	"strconv"
+	"time"
 
 	"github.com/samber/lo"
 )
@@ -14,14 +15,14 @@ import (
 type ClawMachine struct {
 	ax, ay int
 	bx, by int
-	x, y   int
+	px, py int
 }
 
 func main() {
 	// INPUTS
 
 	input_path := "./input.txt"
-	// input_path := "./input-sample.txt"
+	// input_path = "./input-sample.txt"
 
 	_, source_file_path, _, _ := runtime.Caller(0)
 	source_dir := filepath.Dir(source_file_path)
@@ -57,15 +58,18 @@ func main() {
 
 	// PROBLEM 1
 
+	start := time.Now()
+
 	tokens := 0
 
 	for _, cm := range claw_machines {
+		// Cramer's rule
 		l := (cm.bx * cm.ay) - (cm.by * cm.ax)
-		r := (cm.x * cm.ay) - (cm.y * cm.ax)
+		r := (cm.px * cm.ay) - (cm.py * cm.ax)
 		b := r / l
-		a := (cm.x - (cm.bx * b)) / cm.ax
+		a := (cm.px - (cm.bx * b)) / cm.ax
 
-		fmt.Println("ax", cm.ax, "ay", cm.ay, "bx", cm.bx, "by", cm.by, "x", cm.x, "y", cm.y)
+		fmt.Println("ax", cm.ax, "ay", cm.ay, "bx", cm.bx, "by", cm.by, "px", cm.px, "py", cm.py)
 		fmt.Println("l", l, "r", r, "a", a, "b", b)
 
 		if (a > 100) || (b > 100) {
@@ -77,7 +81,7 @@ func main() {
 		x := a*cm.ax + b*cm.bx
 		y := a*cm.ay + b*cm.by
 
-		if cm.x != x && cm.y != y {
+		if cm.px != x || cm.py != y {
 			fmt.Println("impossible", x, y)
 			fmt.Println()
 			continue
@@ -91,5 +95,53 @@ func main() {
 		tokens += t
 	}
 
-	fmt.Println("Problem 1 Result:", tokens) // 34787
+	fmt.Println("Problem 1 Result:", tokens, "●", time.Since(start)) // 34787 ● 6.58389ms
+
+	fmt.Println()
+	fmt.Println("--------------------------------------------------------------------------------")
+	fmt.Println()
+
+	// PROBLEM 2
+
+	start = time.Now()
+
+	tokens = 0
+
+	for _, cm := range claw_machines {
+		cm.px += 10000000000000
+		cm.py += 10000000000000
+
+		// Cramer's rule
+		l := (cm.bx * cm.ay) - (cm.by * cm.ax)
+		r := (cm.px * cm.ay) - (cm.py * cm.ax)
+		b := r / l
+		a := (cm.px - (cm.bx * b)) / cm.ax
+
+		fmt.Println("ax", cm.ax, "ay", cm.ay, "bx", cm.bx, "by", cm.by, "px", cm.px, "py", cm.py)
+		fmt.Println("l", l, "r", r, "a", a, "b", b)
+
+		if a < 0 || b < 0 {
+			fmt.Println("negative")
+			fmt.Println()
+			continue
+		}
+
+		x := a*cm.ax + b*cm.bx
+		y := a*cm.ay + b*cm.by
+
+		if cm.px != x || cm.py != y {
+			fmt.Println("impossible", "x", x, "y", y)
+			fmt.Println()
+			continue
+		}
+
+		t := a*3 + b
+
+		fmt.Println("Yay!", "t", t)
+		fmt.Println()
+
+		tokens += t
+	}
+
+	fmt.Println("Problem 2 Result:", tokens, "●", time.Since(start)) // 85644161121698 ● 2.445132ms
 }
