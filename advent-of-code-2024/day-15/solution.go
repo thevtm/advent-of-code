1407ad1470afc1dcfda6e52d8a32e370129fd6ae
Vinicius Tabille Manjabosco <978715+thevtm@users.noreply.github.com>
2026-04-10 15:38:37 +0200

    2024: Day 15 - Part 2

--- advent-of-code-2024/day-15/solution.go
+++ advent-of-code-2024/day-15/solution.go
@@ -6,6 +6,9 @@ import (
 	"path/filepath"
 	"runtime"
 	"strings"
+	"time"
+
+	"sort"
 
 	"github.com/samber/lo"
 )
@@ -28,8 +31,9 @@ func main() {
 	// INPUTS
 
 	input_path := "./input.txt"
-	// input_path := "./input-sample-small.txt"
-	// input_path := "./input-sample-large.txt"
+	// input_path = "./input-sample-small.txt"
+	// input_path = "./input-sample-large.txt"
+	// input_path = "./input-sample-problem-2.txt"
 
 	_, source_file_path, _, _ := runtime.Caller(0)
 	source_dir := filepath.Dir(source_file_path)
@@ -81,6 +85,8 @@ func main() {
 	fmt.Println()
 
 	// PROBLEM 1
+
+	start := time.Now()
 
 	get_warehouse_tile := func(position Position) rune { return warehouse[position.y][position.x] }
 	set_warehouse_tile := func(position Position, value rune) { warehouse[position.y][position.x] = value }
@@ -156,5 +162,194 @@ func main() {
 		}
 	}
 
-	fmt.Println("Problem 1 Result:", boxes_coordinates_sum) // 1514333
+	fmt.Println("Problem 1 Result:", boxes_coordinates_sum, "●", time.Since(start)) // 1514333 ● 2.279269435s
+
+	fmt.Println()
+	fmt.Println("--------------------------------------------------------------------------------")
+	fmt.Println()
+
+	// PROBLEM 1
+
+	start = time.Now()
+
+	var push_boxes_horizontal func(position Position, direction rune) bool
+	push_boxes_horizontal = func(position Position, direction rune) bool {
+		tile := get_warehouse_tile(position)
+
+		if tile == '#' {
+			return false
+		}
+
+		if tile == '.' {
+			return true
+		}
+
+		if tile == '[' || tile == ']' {
+			next_position := position.plus(directions[direction])
+
+			if push_boxes_horizontal(next_position, direction) == false {
+				return false
+			}
+
+			set_warehouse_tile(position, '.')
+			set_warehouse_tile(next_position, tile)
+
+			return true
+		}
+
+		panic(fmt.Sprintf("Unknown tile: %s", string(tile)))
+	}
+
+	var push_boxes_vertical func(starting_position Position, direction rune) bool
+	push_boxes_vertical = func(starting_position Position, direction rune) bool {
+		fmt.Println("[push_boxes_vertical]", "starting_position", starting_position, "direction", string(direction))
+
+		// 1. Check if we can move
+		visited_positions := make(map[Position]bool)
+
+		stack := make([]Position, 0)
+		stack = append(stack, starting_position)
+
+		boxes_positions := make([]Position, 0)
+
+		for len(stack) > 0 {
+			position := stack[len(stack)-1]
+			stack = stack[:len(stack)-1]
+
+			if visited_positions[position] {
+				fmt.Println("\t", "position", position, "already visited")
+				continue
+			}
+
+			tile := get_warehouse_tile(position)
+
+			fmt.Println("\t", "position", position, "tile", string(tile))
+
+			if tile == '.' {
+				continue
+			}
+
+			if tile == '#' {
+				fmt.Println("\t", "Blocked!")
+				return false
+			}
+
+			if tile == ']' || tile == '[' {
+				var left_side_pos Position
+				var right_side_pos Position
+
+				if tile == '[' {
+					left_side_pos = position
+					right_side_pos = position.plus(directions['>'])
+				} else {
+					left_side_pos = position.plus(directions['<'])
+					right_side_pos = position
+				}
+
+				visited_positions[left_side_pos] = true
+				visited_positions[right_side_pos] = true
+
+				stack = append(stack, left_side_pos.plus(directions[direction]))
+				stack = append(stack, right_side_pos.plus(directions[direction]))
+
+				boxes_positions = append(boxes_positions, left_side_pos)
+
+				continue
+			}
+
+			panic(fmt.Sprintf("Unknown tile: %s", string(tile)))
+		}
+
+		// 2. Move boxes (must process boxes furthest in the direction of movement first)
+		sort.Slice(boxes_positions, func(i, j int) bool {
+			if direction == '^' {
+				return boxes_positions[i].y < boxes_positions[j].y
+			}
+			return boxes_positions[i].y > boxes_positions[j].y
+		})
+
+		for i := 0; i < len(boxes_positions); i++ {
+			left_position := boxes_positions[i]
+			right_position := left_position.plus(directions['>'])
+
+			next_left_position := left_position.plus(directions[direction])
+			next_right_position := next_left_position.plus(directions['>'])
+
+			set_warehouse_tile(next_left_position, get_warehouse_tile(left_position))
+			set_warehouse_tile(next_right_position, get_warehouse_tile(right_position))
+
+			set_warehouse_tile(left_position, '.')
+			set_warehouse_tile(right_position, '.')
+		}
+
+		return true
+	}
+
+	push_boxes := func(robot_position Position, direction rune) bool {
+		switch direction {
+		case '<', '>':
+			return push_boxes_horizontal(robot_position, direction)
+		case '^', 'v':
+			return push_boxes_vertical(robot_position, direction)
+		}
+
+		panic(fmt.Sprintf("Unknown direction: %s", string(direction)))
+	}
+
+	// 1. Modify Warehouse
+	warehouse = make([][]rune, len(warehouse_lines))
+
+	for i, line := range warehouse_lines {
+		new_line := strings.ReplaceAll(line, "O", "[]")
+		new_line = strings.ReplaceAll(new_line, ".", "..")
+		new_line = strings.ReplaceAll(new_line, "@", "@.")
+		new_line = strings.ReplaceAll(new_line, "#", "##")
+
+		warehouse[i] = []rune(new_line)
+	}
+
+	warehouse_width *= 2
+
+	print_rune_grid(warehouse)
+	println()
+
+	// 2. Execute moves
+	robot_position = robot_starting_position
+	robot_position.x *= 2
+
+	for i, direction := range moves {
+		fmt.Println("Move", i, string(direction), "robot_position", robot_position)
+
+		next_robot_position := robot_position.plus(directions[direction])
+
+		if push_boxes(next_robot_position, direction) {
+			set_warehouse_tile(robot_position, '.')
+			set_warehouse_tile(next_robot_position, '@')
+
+			fmt.Printf("Moved from %v to %v\n", robot_position, next_robot_position)
+
+			robot_position = next_robot_position
+		} else {
+			fmt.Println("Unable to move.")
+		}
+
+		print_rune_grid(warehouse)
+
+		fmt.Println()
+	}
+
+	// 3. Calculate GPS coordinates
+	boxes_coordinates_sum = 0
+
+	for y, row := range warehouse {
+		for x, cell := range row {
+			if cell == '[' {
+				boxes_coordinates_sum += y*100 + x
+			}
+		}
+	}
+
+	fmt.Println()
+
+	fmt.Println("Problem 2 Result:", boxes_coordinates_sum, "●", time.Since(start)) // 1528453 ● 2.705151983s
 }
